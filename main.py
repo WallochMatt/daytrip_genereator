f30f860a9f117ef75964842de9e1f167d6d1c7aa
WallochMatt <matthewrwalloch@gmail.com>
2022-08-17 11:01:35 -0500

    Fixed an issue resulting in incorrect rolls across multiplw lists.

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ list_of_restaurants = ["Kyoto Sushi", "Tanpopo Ramen", "Mah Raja", "Culver's", "
 list_of_transports = ["car", "Uber", "carriage", "bike", "unicorn"]
 list_of_entertainment = ["watch a movie", "go jet skiing", "hit the range", "LARP", "play D&D"]
 
-temporary_check = "" #this is part of my copy prevention/exclusion
+master_list = [list_of_destinations, list_of_restaurants, list_of_transports, list_of_entertainment]
 
-def select_choice(list, previous_value):#i might be able to set a parameter for the index, so the function will rerun if it chooses the index, ie repeated choice
+def select_choice(list, previous_value):
     new_value = random.choice(list)
     if previous_value == new_value:
-        #temporary_check = rand_destin #second part of excluding disliked option
-        return select_choice(list, previous_value)#may need to remove or change argument back in here
+        return select_choice(list, previous_value)
     return new_value
+trip_maker = [select_choice(list_of_destinations, ""), select_choice(list_of_transports, ""),select_choice(list_of_entertainment, ""), select_choice(list_of_restaurants, "")] # need ()?
 
-#if I have time I could create a default parameter
-master_list = [select_choice(list_of_destinations, ""), select_choice(list_of_transports, ""),select_choice(list_of_entertainment, ""), select_choice(list_of_restaurants, "")] # need ()?
+                                                #   ^  Empty string indicating previous value, which goes in empty, checks only after initial fill in the functio
+
 complete = False
 
 while not complete:
-    print(f"How about a trip to the {master_list[0]}, by way of {master_list[1]} where we can stop to {master_list[2]} and eat at {master_list[3]}.")
-    for index in master_list:
-        happy = int(input("How does that sound?: "))
+    print(f"How about a trip to the {trip_maker[0]}, by way of {trip_maker[1]} where we can stop to {trip_maker[2]} and eat at {trip_maker[3]}.")
+    for index in range (len(trip_maker)):
+        happy = int(input(f"How does {trip_maker[index]} sound?: ")) #make happy a one time check, then check specifics 
+        
         if happy == 0: #YES
             pass
         if happy == 1: #NO
-            temporary_check = index #probably do not need temp check anymore
+            trip_maker[index] = select_choice(master_list[index], trip_maker[index])
+            
 
 
 
